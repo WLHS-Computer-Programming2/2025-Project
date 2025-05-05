00e65566bb5fd171a0f906651a7a6c2366d8c5e1
smithbwlhs <smithb@wlwv.k12.or.us>
2025-05-05 15:39:25 -0700

    middle of lesson 3

--- yip_lesson.py
+++ yip_lesson.py
@@ -10,15 +10,25 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Smith Intro Game") # title of window
 clock = pygame.time.Clock() # used for frame rate
+
+# left (x), top (y), width, height
 player = pygame.Rect(150,150,50,50)
 
 def draw():
     window.fill((20,18,167)) # rgb tuple fills background
-    
     pygame.draw.rect(window,(2,239,238),player)
 
 while True: # game loop
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN: # if key is pressed down and released
+            if event.key in (pygame.K_UP, pygame.K_w):
+                player.y -= 10
+            if event.key in (pygame.K_DOWN, pygame.K_s):
+                player.y += 10
+            if event.key in (pygame.K_LEFT, pygame.K_a):
+                player.x -= 10
+            if event.key in (pygame.K_RIGHT, pygame.K_d):
+                player.x += 10
         if event.type == pygame.QUIT: # user clicks the X button in window
             pygame.quit()
             exit()
